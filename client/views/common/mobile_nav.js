f4a8752f7e79d06a62744d9c45434a9a013afaf4
AJ <alexander.renold@gmail.com>
2013-11-16 11:02:42 -0800

    updated settings, added categories to mobile-nav

--- client/views/common/mobile_nav.js
+++ client/views/common/mobile_nav.js
@@ -1,3 +1,15 @@
+Template.mobile_nav.helpers({
+  hasCategories: function(){
+    return Categories.find().count();
+  },
+  categories: function(){
+    return Categories.find();
+  },
+  categoryLink: function () {
+    return getCategoryUrl(this.slug);
+  }
+});
+
 Template.mobile_nav.events = {
   'click .mobile-nav a':function(event){
     $('body').toggleClass('mobile-nav-open');
