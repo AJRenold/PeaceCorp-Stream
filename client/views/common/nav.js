47eb009132a5193d824ecdb78159f8a5b75315e0
AJ <alexander.renold@gmail.com>
2013-11-16 09:04:34 -0800

    fixed password appearing in search box

--- client/views/common/nav.js
+++ client/views/common/nav.js
@@ -2,21 +2,6 @@ Template.nav.helpers({
   site_title: function(){
     return getSetting('title');
   },
-  logo_url: function(){
-    return getSetting('logoUrl');
-  },
-  logo_height: function(){
-    return getSetting('logoHeight');
-  },
-  logo_width: function(){
-    return getSetting('logoWidth');
-  },
-  logo_top: function(){
-    return Math.floor((70-getSetting('logoHeight'))/2);
-  },  
-  logo_offset: function(){
-    return -Math.floor(getSetting('logoWidth')/2);
-  },
   intercom: function(){
     return !!getSetting('intercomId');
   },
@@ -72,7 +57,7 @@ Template.nav.events = {
   },
   'keyup, change, search .search-field': function(e){
     e.preventDefault();
-    var val = $(e.target).val(),
+    var val = $('#search').val(),
         $search = $('.search');
     Session.set('searchQuery', val);
     if(val==''){
